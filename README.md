a3ae723e33e171c365338509c136d0c3bc6f0c37
Walassi Silva <91761745+WalassiSilva@users.noreply.github.com>
2023-07-23 22:57:04 -0300

    Add files via upload
    Adicionando projeto

---
+++ README.md
@@ -0,0 +1,17 @@
+# README Tela de login com mensagem de sucesso
+Esse projeto tem o intuito de fixar conhecimentos adquiridos e me desafiar a explorar outros conhecimentos ✍
+
+[<img src="./studing.gif" alt="gif de um rapaz estudando">](https://github.com/WalassiSilva/)
+
+## Objetivos
+- (x) Fazer o layout responsivo
+- (x) Validar o valor do input
+- (x) Exibir mensagem de erro
+- (x) Trocar de tela ao preencher corretamente
+- (x) Exibir e ocultar elementos
+
+
+## Tecnologias utilizadas
+- HTML
+- CSS
+- JS
---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('.form');
+const btnSubscribe = document.querySelector('#btn-subscribe');
+const input = document.querySelector('#email');
+const btnDismiss = document.querySelector('#btn-dismiss');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const container1 = document.querySelector('.container');
+    const container2 = document.querySelector('.container-2');
+    const userEmail = document.querySelector('#user-email');
+
+    if (input.value !== "") {
+        container2.classList.remove('hide');
+        container1.classList.add('hide');
+
+        userEmail.innerText = input.value;
+    }
+    else {
+        form.classList.add('invalid');
+    }
+})
+
+btnDismiss.addEventListener('click', () =>{
+    
+})
